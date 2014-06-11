7ac21c76bca9b61967c06dca7c77c231b76cb038
hu-lihua <hu.lihua@dena.jp>
2014-06-11 23:08:30 +0900

    AnnularProgress

--- SimplePlayer/UI/CustomController/AnnularProgress.swift
+++ SimplePlayer/UI/CustomController/AnnularProgress.swift
@@ -7,8 +7,18 @@
 //
 
 import UIKit
+import QuartzCore
+import Darwin
+
+let pi:CGFloat              = 3.1415926;
 
 class AnnularProgress: UIView {
+    
+    var currentValue : CGFloat  = 0;
+    var maxValue : CGFloat      = 0;
+    
+    var outerRadius:CGFloat     = 0;
+    var innerRadius:CGFloat     = 0;
 
     init(frame: CGRect) {
         super.init(frame: frame)
@@ -17,16 +27,47 @@ class AnnularProgress: UIView {
     
     //
     init(outerRadius:CGFloat, innerRadius:CGFloat) {
-        let frame:CGRect = CGRectMake(0, 0, outerRadius * 2.0, innerRadius * 2.0);
+        let frame:CGRect = CGRectMake(50, 100, outerRadius * 2.0, outerRadius * 2.0);
+        super.init(frame: frame);
+        
+        self.outerRadius = outerRadius;
+        self.innerRadius = innerRadius;
+        
+        self.backgroundColor = UIColor.whiteColor();
+        
+        //println("Search iTunes API at URL \(outerRadius)")
+        NSLog("%f", self.outerRadius);
     }
 
-    /*
+    
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect)
     {
         // Drawing code
+        // draw annular background color
+        var lineWidth = self.outerRadius - self.innerRadius;
+        var processBackgroundPath:UIBezierPath = UIBezierPath();
+        processBackgroundPath.lineWidth = lineWidth;
+        processBackgroundPath.lineCapStyle = kCGLineCapButt;
+        
+        var centerPoint:CGPoint = CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
+        var radius:CGFloat      = (self.bounds.size.width - lineWidth)/2;
+        var startAngle:CGFloat  = ((self.currentValue/self.maxValue) * 2 * pi) - (pi / 2.0);
+        var endAngle:CGFloat    = (2 * pi) - (pi / 2);// + startAngle;
+        processBackgroundPath.addArcWithCenter(centerPoint, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true);
+        UIColor.redColor().set();
+        processBackgroundPath.stroke();
+        
+        // draw progress
+        var processPath:UIBezierPath = UIBezierPath();
+        processPath.lineCapStyle = kCGLineCapButt;
+        processPath.lineWidth = lineWidth;
+        startAngle = -1 * (pi / 2);
+        endAngle = ((self.currentValue/self.maxValue) * 2 * pi) + startAngle;
+        processPath.addArcWithCenter(centerPoint, radius:radius, startAngle:startAngle, endAngle:endAngle, clockwise:true);
+        UIColor.whiteColor().set();
+        processPath.stroke();
     }
-    */
 
 }
--- SimplePlayer/UI/MainViewController.swift
+++ SimplePlayer/UI/MainViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet var songNameLabel : UILabel;
 
     init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -24,13 +26,19 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        songNameLabel.text = "test";
+        
+        var progress : AnnularProgress = AnnularProgress(outerRadius: 100.0, innerRadius: 50.0);
+        self.view.addSubview(progress);
+        
+        progress.maxValue       = 100.0;
+        progress.currentValue   = 40.0;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // #pragma mark - Navigation
@@ -41,5 +49,7 @@ class MainViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    //
 
 }
